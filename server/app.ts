57755332198428b01fb0a55b367539d263fcb271
Halleys123 <arnavadvik2003@gmail.com>
2026-09-05 02:57:39 +0530

    refactor: Just refactoring app.ts

--- server/app.ts
+++ server/app.ts
@@ -1,28 +1,28 @@
-import ErrorHandler from '@middleware/ErrorHandler.js';
-import InvalidRoute from '@middleware/InvalidRoute.js';
+import ErrorHandler from "@middleware/ErrorHandler.js";
+import InvalidRoute from "@middleware/InvalidRoute.js";
 
-import AuthRouter from '@routes/AuthRoutes.js';
-import GameRouter from '@routes/GameRoutes.js';
+import AuthRouter from "@routes/AuthRoutes.js";
+import GameRouter from "@routes/GameRoutes.js";
 
-import express, { Application } from 'express';
-import cors from 'cors';
-import UserRouter from '@routes/UserRoutes.js';
+import express, { Application } from "express";
+import UserRouter from "@routes/UserRoutes.js";
+import cors from "cors";
 
 const app: Application = express();
 
 app.use(
   cors({
-    origin: '*',
-  })
+    origin: "*",
+  }),
 );
 
 app.use(express.json());
 
-app.use('/api/v1/auth', AuthRouter);
-app.use('/api/v1/game', GameRouter);
-app.use('/api/v1/user', UserRouter);
+app.use("/api/v1/auth", AuthRouter);
+app.use("/api/v1/game", GameRouter);
+app.use("/api/v1/user", UserRouter);
 
-app.all('{*any}', InvalidRoute);
+app.all("{*any}", InvalidRoute);
 app.use(ErrorHandler);
 
 export default app;
